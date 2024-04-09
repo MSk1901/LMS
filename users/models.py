6c0d097c4c9428aaa77f0a96097e847482dc684c
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-09 14:03:21 +0300

    fix verbose name for city

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True, verbose_name='почта')
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
-    city = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
+    city = models.CharField(max_length=100, verbose_name='город', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
 
     USERNAME_FIELD = 'email'
