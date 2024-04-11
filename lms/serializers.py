d0b9c2644a54e84137379d0091f3f925ac195a66
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-11 17:29:31 +0300

    add number of lessons to course serializer

--- lms/serializers.py
+++ lms/serializers.py
@@ -5,9 +5,14 @@ from users.models import User
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lesson_count(self, instance):
+        return instance.lessons.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
