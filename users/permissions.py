e0f647ef12d7f9e81bda9125920acfe0a85489cc
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-15 22:45:42 +0300

    add OwnerPermission

--- users/permissions.py
+++ users/permissions.py
@@ -4,3 +4,9 @@ from rest_framework.permissions import BasePermission
 class ManagerPermission(BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name='manager').exists()
+
+
+class OwnerPermission(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
