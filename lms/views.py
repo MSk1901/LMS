1af81becd75a2ad789327971a7df8e3b5a65a287
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-15 20:49:02 +0300

    add manager permissions to views

--- lms/views.py
+++ lms/views.py
@@ -2,11 +2,17 @@ from rest_framework import viewsets, generics
 
 from lms.models import Course, Lesson
 from lms.serializers import CourseSerializer, LessonSerializer
+from users.permissions import ManagerPermission
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def get_permissions(self):
+        if self.action in ('list', 'retrieve', 'update', 'partial_update'):
+            self.permission_classes = [ManagerPermission]
+        return [permission() for permission in self.permission_classes]
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
@@ -16,16 +22,19 @@ class LessonCreateAPIView(generics.CreateAPIView):
 class LessonListAPIView(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [ManagerPermission]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [ManagerPermission]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [ManagerPermission]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
