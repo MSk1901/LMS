106beb4c374c65bcee67123e3d6ae081195cd913
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-15 18:57:58 +0300

    change User serializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,10 +3,29 @@ from rest_framework import serializers
 from users.models import User, Payment
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'phone', 'city', 'avatar', 'first_name', 'last_name')
+        fields = ('id', 'email', 'phone', 'city', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            phone=validated_data.get('phone'),
+            city=validated_data.get('city')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.city = validated_data.get('city', instance.city)
+        instance.set_password(validated_data.get('password'))
+        instance.save()
+        return instance
 
 
 class PaymentSerializer(serializers.ModelSerializer):
