ce0c9660eb1f82fa3b2d2d4b1c9d46d6e3fde61b
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-15 18:58:09 +0300

    add User ViewSet

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.filters import OrderingFilter
 
-from users.models import Payment
-from users.serializers import PaymentSerializer
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class PaymentListAPIView(generics.ListAPIView):
@@ -12,3 +12,8 @@ class PaymentListAPIView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('course_subject', 'lesson_subject', 'method')
     ordering_fields = ['date']
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
