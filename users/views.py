5bba539779eac1e4b7f37e6a1d80c6ec8817cbb6
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-25 19:40:07 +0300

    add PaymentCreateAPIView

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, serializers
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, UserSerializer
+from users.services import create_stripe_product_with_price, create_stripe_session
 
 
 class PaymentListAPIView(generics.ListAPIView):
@@ -13,6 +14,33 @@ class PaymentListAPIView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('course_subject', 'lesson_subject', 'method')
     ordering_fields = ['date']
+
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    serializer_class = PaymentSerializer
+
+    def perform_create(self, serializer):
+        payment = serializer.save(user=self.request.user)
+        if payment.course_subject:
+            product = payment.course_subject
+        elif payment.lesson_subject:
+            product = payment.lesson_subject
+        else:
+            error_message = {
+                "course_subject": [
+                    "One of these fields is required."
+                ],
+                "lesson_subject": [
+                    "One of these fields is required."
+                ]
+            }
+            raise serializers.ValidationError(error_message)
+        if payment.method == 'card':
+            stripe_price = create_stripe_product_with_price(product.title, payment.amount)
+            session_id, payment_link = create_stripe_session(stripe_price)
+            payment.session_id = session_id
+            payment.link = payment_link
+        payment.save()
 
 
 class UserViewSet(viewsets.ModelViewSet):
