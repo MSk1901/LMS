a7ac0e451a885ab746f7c34e1e44727e9e414948
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-25 19:38:32 +0300

    add admin display for Payment

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Payment
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'is_superuser')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('date', 'amount')
