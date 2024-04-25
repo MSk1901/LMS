4c6d80b4c71b7426700ab502ec555815d1192691
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-25 19:40:15 +0300

    add url for PaymentCreateAPIView

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView, UserViewSet
+from users.views import PaymentListAPIView, UserViewSet, PaymentCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -15,4 +15,6 @@ urlpatterns = [
 
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('payment/', PaymentCreateAPIView.as_view(), name='payment'),
+
 ] + router.urls
