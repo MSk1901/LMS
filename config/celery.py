0f75bfbea30462cdca54b52977db0b749b97589d
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-04-29 19:33:29 +0300

    refactor celery settings

--- config/celery.py
+++ config/celery.py
@@ -2,13 +2,10 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# Создание экземпляра объекта Celery
 app = Celery('config')
 
-# Загрузка настроек из файла Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
 app.autodiscover_tasks()
